2b782b8f352b94ecb0f6ab381ad81733f405dbed
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-24 16:29:43 +0300

    RandomWord

---
+++ src/RandomWord.java
@@ -0,0 +1,19 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        int index = 0;
+        String champ = "";
+
+        while (!StdIn.isEmpty()){
+            String curr = StdIn.readString();
+            if (StdRandom.bernoulli(1 / (index + 1.0))){
+                champ = curr;
+            }
+            index++;
+        }
+        StdOut.println(champ);
+    }
+}
